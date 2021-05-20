5fccb315cfd1c1bcdd88cc5fc66e630db5924ac9
W3ndig0u0 <jingxiang.xu@elev.ga.ntig.se>
2021-05-20 10:53:52 +0200

    asd

---
+++ Return/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Return
+{
+  class Program
+  {
+    public static string[] fruits = { "Äpple", "Päron", "Banan", "Citron", "Annanas", "Apelsin", "Kiwi", "Mango" };
+    public static void Main()
+    {
+      Console.WriteLine("Välj en av dessa frukter");
+
+      for (int i = 0; i < fruits.Length; i++)
+      {
+        Console.WriteLine(i + 1 + ". " + fruits[i]);
+      }
+      ArrayIndex(int.Parse(Console.ReadLine()));
+    }
+    public static int ArrayIndex(int i)
+    {
+      string fruit = fruits[i - 1];
+      Console.WriteLine($"Du har valt {fruit}");
+      return i;
+    }
+  }
+}
