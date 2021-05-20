84ca899a9caf2a917c898f143eb36a8588facb9e
W3ndig0u0 <jingxiang.xu@elev.ga.ntig.se>
2021-05-20 16:54:25 +0200

    Array

--- Return/Program.cs
+++ Return/Program.cs
@@ -4,22 +4,39 @@ namespace Return
 {
   class Program
   {
-    public static string[] fruits = { "Äpple", "Päron", "Banan", "Citron", "Annanas", "Apelsin", "Kiwi", "Mango" };
+    public static string[] fruits = GetFruits();
     public static void Main()
     {
-      Console.WriteLine("Välj en av dessa frukter");
+      // !Ställer frågan
+      Question(fruits);
+    }
 
+    public static string[] Question(string[] array)
+    {
+
+      Console.WriteLine("Välj en av dessa frukter");
+      // !Loopar igenom Arrayen
       for (int i = 0; i < fruits.Length; i++)
       {
         Console.WriteLine(i + 1 + ". " + fruits[i]);
       }
+
+      // !Gör svaret till en int som blir Int arrayIndex
       ArrayIndex(int.Parse(Console.ReadLine()));
+
+      return array;
     }
-    public static int ArrayIndex(int i)
+    public static int ArrayIndex(int arrayIndex)
     {
-      string fruit = fruits[i - 1];
+      // !Skriver ut vilket frukt som användaren valde
+      string fruit = fruits[arrayIndex - 1];
       Console.WriteLine($"Du har valt {fruit}");
-      return i;
+      return arrayIndex;
+    }
+    static string[] GetFruits()
+    {
+      // !Retruanas arrayen
+      return new[] { "Äpple", "Päron", "Banan", "Citron", "Annanas", "Apelsin", "Kiwi", "Mango" };
     }
   }
 }
